a641eddcc249142f37dd9bd686665b7ba6a8a0e7
Esa Varemo <esa@kuivanto.fi>
2019-03-17 22:14:53 +0200

    Initialize DB also in WSGI mode

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,3 +1,10 @@
 
 # gunicorn wants to find 'application' to run
-from server import app as application
+from server import app
+from database import init_db
+
+init_db()
+
+
+def application(environ, start_response):
+    return app(environ, start_response)
