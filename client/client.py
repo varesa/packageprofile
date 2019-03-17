ac49699c91d2b521b298df0cb5eb8e6a7b6f4435
Esa Varemo <esa@kuivanto.fi>
2019-03-17 22:18:16 +0200

    Add a local fallback URL if config is missing

--- client/client.py
+++ client/client.py
@@ -5,15 +5,23 @@ import socket
 from packagemanager import get_packages
 
 
+CONFIG_LOCATION = "/etc/packageprofile.conf"
+FALLBACK_URL = "http://localhost:8000/publish"
+
+
 def main():
     hostname = socket.gethostname()
     assert not hostname.startswith("localhost"), "Could not get real hostname"
     assert len(hostname.split('.')) > 1, "Could not get FQDN"
 
-    config = configparser.ConfigParser()
-    config.read("/etc/packageprofile.conf")
-    url = config['server']['url']
-    print("URL: {}".format(url))
+    try:
+        config = configparser.ConfigParser()
+        config.read(CONFIG_LOCATION)
+        url = config['server']['url']
+        print("URL: {}".format(url))
+    except KeyError:
+        print("No config file found at " + CONFIG_LOCATION + ", falling back to " + FALLBACK_URL)
+        url = FALLBACK_URL
 
     packages = get_packages()
     r = requests.post(url, json={"hostname": hostname, "packages": packages})
